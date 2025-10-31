3057bf89d831edd28ec36f1b1359e44cbb7332de
f-lapinski <lapinski.filipp@gmail.com>
2025-10-31 10:36:44 +0100

    Add Roles on register Add Authorization by roles

--- NoExp.Presentation/Program.cs
+++ NoExp.Presentation/Program.cs
@@ -40,6 +40,7 @@ namespace NoExp.Presentation
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddSignInManager()
                 .AddDefaultTokenProviders();
